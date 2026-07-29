db72564e243a952dbc06e03ac282e0f97f99fc53
Dyeizom <155914631+Dyeizom@users.noreply.github.com>
2026-07-28 22:07:51 -0300

    Create 03 - Exercicio.py

---
+++ 03 - Exercicio.py
@@ -0,0 +1,8 @@
+#MEDIA ALUNO 
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+media = (n1 + n2) / 2 #SOMA OS VALORES E DIVIDE POR 2
+
+print(f'Sua média foi {media}')
