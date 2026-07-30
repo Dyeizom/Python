b43e4bec6a9f3598bc743204ed3d7cfe91d806c0
Dyeizom <155914631+Dyeizom@users.noreply.github.com>
2026-07-29 21:26:18 -0300

    Rename 8.py to 08.py

--- 8.py
+++
@@ -1,5 +0,0 @@
-salario = float(input('Qual valor do seu salario '))
-
-aumento = salario + (salario*15/100)
-
-print('Você teve um aumento no salario de 15% é seu salario agora e R$ {:.2f}' .format(aumento))
